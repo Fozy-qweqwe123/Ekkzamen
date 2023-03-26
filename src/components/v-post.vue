1ac38eb71f4bfc245e42eea8ff97d2a47e5c609f
Rubik Agamiryan <axperrubik@gmail.com>
2023-03-26 20:55:10 +0300

    удалил лишние компоненты

--- src/components/v-post.vue
+++ src/components/v-post.vue
@@ -1,26 +1,41 @@
 <template>
-  <div>
-    <h1 class="news">Новости</h1>
-    <ul>
-      <li v-for="post in posts" :key="post.id">
-        <h3>{{ post.title }}</h3>
-        <p>{{ post.content }}</p>
-        <button @click="likePost(post_id)">Лайкнуть</button>
-        <button @click="showComments(post)">Комментарии ({{ post.comments.length }})</button>
-        <div v-show="post.showComments">
-          <form @submit.prevent="addComment(post)">
-            <input type="text" v-model="newComment">
-            <button>Добавить комментарий</button>
-          </form>
-          <ul>
-            <li v-for="comment in post.comments" :key="comment.id">
-              <p>{{ comment.content }}</p>
-              <button @click="likeComment(post, comment)">Лайaкнуть</button>
-            </li>
-          </ul>
+  <div class="social-feed">
+    <h2>Новости</h2>
+    <div v-for="post in posts" :key="post.id" class="post">
+      <div class="post-header">
+        <img :src="post.avatar" alt="Avatar" class="avatar">
+        <div class="author-info">
+          <h3>{{ post.author }}</h3>
+          <p>{{ post.timestamp }}</p>
         </div>
-      </li>
-    </ul>
+      </div>
+      <div class="post-content">
+        <p>{{ post.content }}</p>
+        <img v-if="post.image" :src="post.image" alt="Post Image" class="post-image">
+      </div>
+      <div class="post-actions">
+        <button @click="likePost(post.id)">
+          <i class="far fa-heart" :class="{ 'fas': post.liked }"></i>
+          <span>{{ post.likes }}</span>
+        </button>
+      </div>
+      <div class="comments">
+        <h4>Comments</h4>
+        <ul>
+          <li v-for="comment in post.comments" :key="comment.id">
+            <div class="comment-info">
+              <h5>{{ comment.author }}</h5>
+              <p>{{ comment.timestamp }}</p>
+            </div>
+            <p>{{ comment.content }}</p>
+          </li>
+        </ul>
+        <form @submit.prevent="addComment(post.id)">
+          <input type="text" v-model="commentText" placeholder="Add a comment...">
+          <button type="submit">Post</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -31,56 +46,174 @@ export default {
       posts: [
         {
           id: 1,
-          title: 'Тимофей Сенин ',
-          content: 'Привет, друзья! Хочу рассказать вам про свою новую книгу, которую недавно выпустил. Это история о моих приключениях в путешествиях по всему миру. Я побывал в самых разных уголках планеты и увидел множество удивительных вещей. Расскажу о том, как я пересекал Атлантику на паруснике, плавал с акулами на Гавайях и восхищался красотами Новой Зеландии. Надеюсь, вы захотите прочитать эту книгу и узнать больше о моих приключениях. Спасибо за внимание!',
-          likes: 2,
-          comments: [],
-          showComments: false,
+          author: "Иван Устинов",
+          avatar: "https://via.placeholder.com/50",
+          timestamp: "2 hours ago",
+          content: "Сегодня была прекрасная погода",
+          image: "https://via.placeholder.com/800x400",
+          likes: 10,
+          liked: false,
+          comments: [
+            {
+              id: 1,
+              author: "Егор Пахарев",
+              timestamp: "1 hour ago",
+              content: "прекрасное фото!"
+            }
+          ]
         },
         {
           id: 2,
-          title: 'Пост 2',
-          content: 'Содержимое поста 2',
-          likes: 0,
-          comments: [],
-          showComments: false,
-        },
+          author: "Тимофей Сенин",
+          avatar: "https://via.placeholder.com/50",
+          timestamp: "4 hours ago",
+          content: "Вот бы там побывать.",
+          image: null,
+          likes: 5,
+          liked: false,
+          comments: []
+        }
       ],
-      newComment: '',
-    }
+      commentText: ""
+    };
   },
   methods: {
-    likePost(post) {
-      post.likes++
+    likePost(postId) {
+      const post = this.posts.find(post => post.id === postId);
+      post.liked = !post.liked;
+      post.likes += post.liked ? 1 : -1;
     },
-    showComments(post) {
-      post.showComments = !post.showComments
-    },
-    addComment(post) {
+    addComment(postId) {
+      const post = this.posts.find(post => post.id === postId);
       post.comments.push({
         id: post.comments.length + 1,
-        content: this.newComment,
-        likes: 0,
-      })
-      this.newComment = ''
-    },
-    likeComment(post, comment) {
-      comment.likes++
-    },
-  },
-}
+        author: "You",
+        timestamp: "just now",
+        content: this.commentText
+      });
+      this.commentText = "";
+    }
+  }
+};
 </script>
-<style scoped>
-  li {
-    margin-bottom: 20px;
-    padding: 20px;
-    background-color: #fff;
-    border: 2px solid #ddd;
-    border-radius: 1%;
-  }
-  .news  {
-    display: flex;
-    align-items:center ;
-    justify-content: center;
-  }
+
+<style>
+.social-feed {
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.social-feed h2 {
+  font-size: 1.5rem;
+  text-align: center;
+}
+
+.post {
+  border: 1px solid #ccc;
+  margin-bottom: 1rem;
+  padding: 1rem;
+}
+
+.post-header {
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5rem;
+}
+
+.avatar {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-right: 0.5rem;
+}
+
+.author-info h3 {
+  margin: 0;
+  font-size: 1.2rem;
+}
+
+.author-info p {
+  margin: 0;
+  color: #666;
+}
+
+.post-content {
+  margin-bottom: 0.5rem;
+}
+
+.post-image {
+  width: 100%;
+  margin-top: 0.5rem;
+}
+
+.post-actions {
+  display: flex;
+  justify-content: space-between;
+}
+
+.post-actions button {
+  background-color: transparent;
+  border: none;
+  display: flex;
+  align-items: center;
+  color: #666;
+}
+
+.post-actions button:hover {
+  cursor: pointer;
+  color: #333;
+}
+
+.post-actions i {
+  margin-right: 0.5rem;
+}
+
+.comments {
+  margin-top: 1rem;
+}
+
+.comments h4 {
+  margin: 0;
+  font-size: 1.2rem;
+}
+
+.comments ul {
+  list-style: none;
+  padding: 0;
+}
+
+.comment-info h5 {
+  margin: 0;
+}
+
+.comment-info p {
+  margin: 0;
+  color: #666;
+}
+
+.comments form {
+  display: flex;
+  margin-top: 1rem;
+}
+
+.comments form input[type="text"] {
+  flex: 1;
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 0;
+}
+
+.comments form button[type="submit"] {
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 0;
+  margin-left: 0.5rem;
+}
+
+.comments form button[type="submit"]:hover {
+  cursor: pointer;
+  background-color: #0069d9;
+}
 </style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
-import servicesview from "@/views/v-services-view.vue";
+
 import postview from "@/views/v-post-view.vue"
 import vReviews from '@/components/v-reviews.vue';
 import Vform from '@/components/v-form.vue';
@@ -9,7 +9,7 @@ export default createRouter({
     routes: [
         {path: '/', component: Vform},
         {path: '/post', component: postview},
-        {path: '/Services', component: servicesview},
+       
         {path: '/Reviews', component: vReviews},
     ]
 })
