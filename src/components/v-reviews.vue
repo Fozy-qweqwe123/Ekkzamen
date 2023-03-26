8fe9d72f82e4e8ba5958f505c6c71872c81985a8
Rubik Agamiryan <axperrubik@gmail.com>
2023-03-26 20:28:17 +0300

    asd

--- src/components/v-reviews.vue
+++ src/components/v-reviews.vue
@@ -15,7 +15,7 @@
     <div class="container-review">
         <img src="@/assets/avatarexample.jpg" alt="Avatar" style="width:90px">
         <p><span>Имя пользователя.</span> </p>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+        <p>Lorem ipsum dolor sit amsset, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
     </div>
 
     <div class="container-review">
